dd0db0d2bf4ac72fa66fb19de6f3be1519b26bfc
maks2000-del <max3047380@mail.ru>
2021-10-23 14:03:27 +0300

    4_initialization

--- fluter_game/my_game/lib/main.dart
+++ fluter_game/my_game/lib/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:my_game/game_core/main_loop.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(() {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('data'),
+      ),
+    ));
+  });
 }
